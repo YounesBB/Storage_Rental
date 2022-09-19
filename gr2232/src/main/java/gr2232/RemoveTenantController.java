eb3195b128389d23aaaed216a83aec451dbc3ec4
Magnus Svendsen <magnsven@stud.ntnu.no>
2022-09-19 19:00:26 +0200

    added table and return button

--- gr2232/src/main/java/gr2232/RemoveTenantController.java
+++ gr2232/src/main/java/gr2232/RemoveTenantController.java
@@ -1,12 +1,31 @@
 package gr2232;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import java.io.IOException;
+
 
 public class RemoveTenantController {
-    
+
     @FXML
-    public void goToOverView() throws IOException {
-        App.setRoot("overviewpage");
+    private TableColumn<?, ?> locationColumn;
+
+    @FXML
+    private TableColumn<?, ?> nameColumn;
+
+    @FXML
+    private TableColumn<?, ?> removeColumn;
+
+    @FXML
+    private TableColumn<?, ?> sizeColumn;
+
+    @FXML
+    private TableView<?> tenantTable;
+
+    @FXML
+    private void goToOverView() throws IOException {
+        App.setRoot("login");
     }
+
 }
