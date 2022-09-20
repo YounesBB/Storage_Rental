dc0764d5660a6ce2fad7573a71f725935a8a8253
Magnus Svendsen <magnsven@stud.ntnu.no>
2022-09-20 20:08:24 +0200

    Added basic getter/setters and some encapsulation. View main method to see some testing

---
+++ gr2232/src/main/java/gr2232/Unit.java
@@ -0,0 +1,83 @@
+package gr2232;
+
+public class Unit {
+
+    private boolean isRented = false;
+    private String customerName;
+    private Integer location;
+    private char size;
+
+    public Unit(char size) {
+        setSize(size);
+    }
+
+    public void setSize(char size) {
+        //Check if valid size
+        if (size == 'S' || size == 'M' || size == 'L') {
+            this.size = size;
+        }
+        else {
+            throw new IllegalArgumentException("Unit size must either be S,M or L!");
+        }
+    }
+
+    public char getSize() {
+        return this.size;
+    }
+
+
+    public boolean getIsRented(){
+        return this.isRented;
+    }
+
+    //Toggles isRented boolean
+    public void toggleIsRented() {
+        //Encapsulate customername, remove name is state is set to not rented
+        if (getIsRented()) {
+            setCustomerName("null");
+        }
+        this.isRented = !this.isRented;
+    }
+
+
+    public void setCustomerName(String name) {
+        if (getIsRented()) {
+            this.customerName = name;
+        }
+        else {
+            throw new IllegalStateException("Cannot set customername when the unit is not rented!");
+        }
+    }
+
+    public String getCustomerName() {
+        return this.customerName;
+    }
+
+    public Integer getLocation() {
+        return this.location;
+    }
+
+    //UnitList will get the location when added. Atleast what I have in mind atm
+    public void setLocation(Integer loc) {
+        this.location = loc;
+    }
+
+    //Testing/debugging
+    public static void main(String args[]) {
+        Unit test = new Unit('M');
+        System.out.println(test.getSize());
+        test.setSize('L');
+        System.out.println(test.getSize());
+        
+        //Throws exception as planned
+        //test.setSize('R');
+
+        System.out.println(test.getIsRented());
+        //Throws exception as planned
+        //Must set state to rented before setting customer name
+        //test.setCustomerName("Magnus Svendsen");
+
+    }
+
+
+}
