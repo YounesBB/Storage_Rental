6adafc245f390b51113f93cd3369e85419ae8fa1
Magnus Svendsen <magnsven@stud.ntnu.no>
2022-10-07 02:21:59 +0200

    Added JSON read/write, might still need some encapsulation/try-cath, but works

--- gr2232/src/main/java/module-info.java
+++ gr2232/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module gr2232 {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens gr2232 to javafx.fxml;
+    requires com.google.gson;
+    opens gr2232;
+
     exports gr2232;
 }
