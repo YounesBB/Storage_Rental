c46ab14d92885da1ca5ad9afd2e9b76138e0def1
Younes Bouhmidi <younesb@stud.ntnu.no>
2022-10-11 22:17:29 +0200

    The project should be close to modulazation. Run mvn verify to see error abot spotbugs that need fix

---
+++ gr2232/fxui/src/main/java/gr2232/RemoveTenantController.java
@@ -0,0 +1,83 @@
+package gr2232;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+
+public class RemoveTenantController {
+
+    @FXML
+    private TableColumn<?, ?> locationColumn;
+
+    @FXML
+    private TableColumn<?, ?> nameColumn;
+
+    @FXML
+    private TableColumn<?, ?> removeColumn;
+
+    @FXML
+    private TableColumn<?, ?> sizeColumn;
+
+    @FXML
+    private TableView<?> tenantTable;
+
+    @FXML
+    private ChoiceBox<String> tenantSelector;
+
+    private ObservableList<String> tenantList = FXCollections.observableArrayList();
+
+    @FXML
+    private void goToOverView() throws IOException {
+        App.setRoot("overviewpage");
+    }
+
+    @FXML 
+    void onRemoveTenant() throws IOException {
+        UnitList unitList = new UnitList();
+        String tenantLine = tenantSelector.getValue();
+        if (tenantLine == null) {
+            throw new IllegalArgumentException("Must select a tenant to remove!");
+        } else {
+            String[] tenantData = tenantLine.split(",");
+            Integer location = Integer.parseInt(tenantData[1]);
+
+            Unit unitToRemoveTenant = unitList.getUnitByLocation(location);
+            unitToRemoveTenant.setIsRented(false);
+            unitList.updateUnitByLocation(location, unitToRemoveTenant);
+
+            updateTenantList();
+            tenantSelector.setItems(tenantList);
+    }
+
+    }
+
+    private void updateTenantList() {
+        UnitList unitList = new UnitList();
+        tenantList.clear();
+        for (var unit : unitList.getRentedUnits()) {
+            String name = unit.getCustomerName();
+            Integer location = unit.getLocation();
+            Character size = unit.getSize();
+            String tenantInformation = name + "," + location + "," + size;
+            this.tenantList.add(tenantInformation);
+        }
+
+    }
+
+    @FXML
+    public void initialize() {
+        updateTenantList();
+        tenantSelector.setItems(tenantList);
+        //tenantSelector.setValue("test");
+    }
+
+}
