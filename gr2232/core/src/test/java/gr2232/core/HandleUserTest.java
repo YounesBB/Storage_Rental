36f2a826e9f8e86a640d2fb15c404c51be92defb
Younes Bouhmidi <younesb@stud.ntnu.no>
2022-10-31 17:22:06 +0100

    chore: This is a commit that has the propose of testing conventional commits in VSCODE \nNewline?
    Longer description

--- gr2232/core/src/test/java/gr2232/core/HandleUserTest.java
+++ gr2232/core/src/test/java/gr2232/core/HandleUserTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class HandleUserTest {
+  //git commit with conventional commits extension 
 
   private HandleUser user;
 
