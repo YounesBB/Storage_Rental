3192f82d32f08cd5a7f865500f9ca45117642ebc
Younes Bouhmidi <younesb@stud.ntnu.no>
2022-10-31 18:10:41 +0100

    feat: Added conventional commits extension
    Added the two vscode extensions that have the propose of making us follow conventional commits standard. We found this was enough for the scope of our project. These extensions were added to the recomended extensions .json file.
    
    closes #51

--- gr2232/core/src/test/java/gr2232/core/HandleUserTest.java
+++ gr2232/core/src/test/java/gr2232/core/HandleUserTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class HandleUserTest {
-  //git commit with conventional commits extension 
 
   private HandleUser user;
 
