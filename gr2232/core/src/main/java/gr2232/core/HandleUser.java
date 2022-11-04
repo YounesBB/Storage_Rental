2ea365f7d9646b4dfddd87dbd6b697bdecda8b62
Magnus Svendsen <magnsven@stud.ntnu.no>
2022-11-04 02:57:11 +0100

    feat: Updated tests for HandleUser

--- gr2232/core/src/main/java/gr2232/core/HandleUser.java
+++ gr2232/core/src/main/java/gr2232/core/HandleUser.java
@@ -3,11 +3,22 @@ package gr2232.core;
 /**
  * A class for handeling which user that uses the system.
  */
-public class HandleUser {
+public class HandleUser implements HandleUserInterface {
   private static boolean isManager;
+  private String username = "admin";
+  private String password = "admin123";
+
 
   public HandleUser() {
-
+    setIsManager(false);
+  }
+  
+  @Override
+  public boolean validateInputs(String inputUsername, String inputPassword) {
+    if((this.username.equals(inputUsername)) && (this.password.equals(inputPassword))) {
+      return true;
+    }
+    return false;
   }
 
   public static void setIsManager(boolean v) {
@@ -17,4 +28,7 @@ public class HandleUser {
   public static boolean getIsManager() {
     return isManager;
   }
+
+
+  
 }
