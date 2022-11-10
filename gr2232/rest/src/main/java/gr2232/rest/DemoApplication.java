d03f0304f2f37e3e42f023aa5a13cafe5271ce14
Younes Bouhmidi <younesb@stud.ntnu.no>
2022-11-10 10:07:57 +0100

    chore: Have made necessary folders and files for Rest folder
    Have made necessary folders and files for Rest folder. The structure for further work is in places.
    
    closes #54

--- gr2232/rest/src/main/java/gr2232/rest/DemoApplication.java
+++ gr2232/rest/src/main/java/gr2232/rest/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-  //start running the server by cd into rest and then mvn spring-boot:run
+  //start running the server by cd into rest and then mvn spring-boot:run.
   //open new terminal and past curl http://localhost:8080/unitlist. This is a GET command 
 
 	public static void main(String[] args) {
