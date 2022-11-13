ae1e54b8db57f15fd85f1519ec1ee4acd1227855
Magnus Svendsen <magnsven@stud.ntnu.no>
2022-11-13 18:43:09 +0100

    feat: loads units from server to local boot after start

--- gr2232/fxui/src/main/java/gr2232/ui/OverviewPageController.java
+++ gr2232/fxui/src/main/java/gr2232/ui/OverviewPageController.java
@@ -1,10 +1,19 @@
 package gr2232.ui;
 
 import java.io.IOException;
-
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Arrays;
+import java.util.List;
 import javax.net.ssl.HandshakeCompletedEvent;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import gr2232.core.HandleUser;
+import gr2232.core.Unit;
+import gr2232.core.UnitList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -24,7 +33,7 @@ public class OverviewPageController {
   private Button unitsOverviewButton;
 
   @FXML
-  void initialize() {
+  void initialize() throws IOException {
     boolean manager = HandleUser.getIsManager();
     boolean rest = HandleUser.getUsesRest();
     if (manager) {
@@ -32,7 +41,33 @@ public class OverviewPageController {
     } else {
       this.addUnitsButton.setDisable(true);
     }
-    System.out.println(rest);
+    /*
+     * Fetches unitlist from server, and sets it locally.  
+     * Does this if there are none units in the program
+     * Assumes fresh boot of program. 
+     */
+    UnitList ul = new UnitList();
+    if (rest && ul.getUnitListEntries().size() == 0) {
+      ObjectMapper mapper = new ObjectMapper();
+      String json = mapper.writeValueAsString("");
+      try {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/unitlist"))
+            .header("Accept", "application/json").header("Content-Type", "application/json").GET().build();
+
+        final HttpResponse<String> response =
+            HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() == 200) {
+          List<Unit> list = Arrays.asList(new GsonBuilder().create().fromJson(response.body(), Unit[].class));
+          ul.clearUnitList();
+          ul.getUnitListEntries().addAll(list);
+        }
+
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
   }
 
   @FXML
