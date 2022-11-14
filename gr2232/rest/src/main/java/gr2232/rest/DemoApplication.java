bded096555142051f06e7930299092e1a10e3cd1
Younes Bouhmidi <younesb@stud.ntnu.no>
2022-11-14 20:16:40 +0100

    docs(rest): Documented our Rest-api by Openapi and swagger
    Documented our Rest-api by spesification of OpenApi and swagger to visulize the documentation/implemetation of Openapi. Did this by adding a dependency and other modifications.
    
    #64

--- gr2232/rest/src/main/java/gr2232/rest/DemoApplication.java
+++ gr2232/rest/src/main/java/gr2232/rest/DemoApplication.java
@@ -1,19 +1,21 @@
 package gr2232.rest;
 
-import gr2232.json.UnitListModule; 
-
+import gr2232.json.UnitListModule;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-//import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.Module;
 
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "UnitList API", version = "3.0", description = "An application used to manage units in a Unit storage house."))
 public class DemoApplication {
 
   //start running the server by cd into rest and then mvn spring-boot:run.
   //open new terminal and past curl http://localhost:8080/unitlist. This is a GET command 
+  //past http://localhost:8080/swagger-ui.html in browser to see the documentation of our api. 
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
