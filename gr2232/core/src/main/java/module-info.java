717feae1b72abc4353f94bb406e186c26c64555e
Magnus Svendsen <magnsven@stud.ntnu.no>
2022-11-18 17:09:08 +0100

    chore:modualized core

--- gr2232/core/src/main/java/module-info.java
+++ gr2232/core/src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module gr2232.core {
   requires transitive com.fasterxml.jackson.databind;
+  requires transitive com.google.gson;
 
+  opens gr2232.json to com.google.gson;
+  opens gr2232.core to com.google.gson;
+  
   exports gr2232.core;
   exports gr2232.json; 
 
