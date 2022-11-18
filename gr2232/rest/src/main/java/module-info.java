afbed3b28410caf3dcbe56936629538e3c64e460
Magnus Svendsen <magnsven@stud.ntnu.no>
2022-11-18 19:27:50 +0100

    chore: fixing rest-tests

--- gr2232/rest/src/main/java/module-info.java
+++ gr2232/rest/src/main/java/module-info.java
@@ -2,8 +2,11 @@ module gr2232.rest {
     requires gr2232.core;
     requires io.swagger.v3.core;
     requires io.swagger.v3.oas.annotations;
+    requires spring.web;
+    requires spring.beans;
+    requires spring.boot;    
+    requires spring.boot.autoconfigure;
+    requires spring.context;
     
-    opens gr2232.rest to org.springframework;
-
-
+    opens gr2232.rest to spring.boot.autoconfigure,spring.boot, spring.beans, spring.web, spring.test;
 }
