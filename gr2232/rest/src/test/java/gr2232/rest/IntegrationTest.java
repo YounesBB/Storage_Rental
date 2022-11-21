d86bcbe4e44a5b3db2f9526f823424e074415b49
Younes Bouhmidi <younesb@stud.ntnu.no>
2022-11-21 02:17:10 +0100

    fix: Fixed unamed module issue
    There was an issue in which build failed becuase there was unamed module that did not open when we tests for rest ran. Had tho change module-info.java for rest and made some changes to InteregationTest.java.
    
    #77

--- gr2232/rest/src/test/java/gr2232/rest/IntegrationTest.java
+++ gr2232/rest/src/test/java/gr2232/rest/IntegrationTest.java
@@ -2,24 +2,29 @@ package gr2232.rest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import gr2232.core.UnitList;
+import gr2232.json.UnitListModule;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(classes = {UnitListService.class, UnitListController.class, UnitListModule.class})
+@EnableAutoConfiguration
 public class IntegrationTest {
 
   @Autowired
   private TestRestTemplate restTemplate;
+  
 
   @Test
   void testGETUnitList() {
